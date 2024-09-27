2aa277136a948360b0e93cc38e8cbfc2e73b3826
norbey095 <norbey095@gmail.com>
2024-09-27 10:17:42 -0500

    Se mejoran constantes

--- src/main/java/com/emazon/api_transaction/infraestructure/util/ConstantsConfigurations.java
+++ src/main/java/com/emazon/api_transaction/infraestructure/util/ConstantsConfigurations.java
@@ -17,6 +17,16 @@ public class ConstantsConfigurations {
     public static final String PASSWORD_EMPTY = "application/json";
     public static final String JWT_SECRET = "${jwt.secret}";
     public static final String AUTHORITIES = "authorities";
+    public static final String NO_DATA_FOUND_EXCEPTION_MESSAGE = "No data found in the database";
+    public static final String ACCESS_DENE = "Access denied";
+    public static final String INCORRECT_DATA = "Incorrect login information";
+    public static final String SERVICE_NOT_AVAILABLE = "The service is not available, please try again later.";
+    public static final String STOCK_CONFLICT = "The article does not exist in the database.";
+    public static final String ERROR_STOCK = "An error occurred while updating the article.";
+    public static final String ACCESS_DENIED = "Access Denied";
+    public static final String BAD_CREDENTIALS = "Bad Credentials";
+    public static final String CLIENT_EXCEPTION = "Client Exception";
+    public static final String FEIGN_EXCEPTION = "Feign Exception";
 
     private ConstantsConfigurations() {
 
